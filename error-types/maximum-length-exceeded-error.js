d20ceb3733bb682bacf81ac17b2d65b1dd27aa13
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-22 18:12:04 +0100

    created MaximumLengthExceededError class extending ValidationError

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,18 @@
 const ValidationError = require('./validation-error');
 
-// Your code here
+class MaximumLengthExceededError extends ValidationError {
+  constructor(excessLength) {
+    const message = excessLength
+      ? `Maximum length exceeded by ${excessLength}`
+      : 'Maximum length exceeded';
+    super(message); // Call the parent class constructor with the message
+    this.name = 'MaximumLengthExceededError'; // Set the name property
+    // Capture stack trace if available
+    if (Error.capturStackTrace) {
+      Error.captureStackTrace(this, MaximumLengthExceededError)
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
