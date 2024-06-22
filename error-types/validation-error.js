072ed2387e8dd2d00e15acffe5e24f1f9bfb36da
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-22 17:57:05 +0100

    created custom ValidationError class

--- error-types/validation-error.js
+++ error-types/validation-error.js
@@ -1,4 +1,13 @@
-// Your code here
+class ValidationError extends Error {
+  constructor(message = 'Invalid input') {
+    super(message); // Call the parent class constructor with the message
+    this.name = 'ValidationError'; // Set the name property to "ValidationError"
+    // capture stack trace if available
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidationError);
+    }
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
